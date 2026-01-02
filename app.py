a083b9f17f6a287b0c6d51990afc8a3d59021d56
Tokiniaina Eddy <eddy9.mah33@gmail.com>
2026-01-02 16:13:28 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+from pymongo import MongoClient
+import os
+from flask_cors import CORS
+
+
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+db = client['SmartAutoCommerce']
+
+def collection(col:str):
+    collection = db[col]
+    df = pd.DataFrame(list(collection.find()))
+    if '_id' in df.columns:        
+        df = df.drop(columns=['_id'])
+    return df.to_dict(orient="records")
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/Amazon')
+def amazon_product():
+    try:
+        return jsonify(collection("Amazon")), 200
+    except Exception as e:
+        return jsonify ({'statut':'erreur', 'erreur' : str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
